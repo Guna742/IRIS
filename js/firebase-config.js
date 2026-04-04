822761cf5e416fe235ee9a1b3efedfb46c4cb8a6
Sudhirnandhan <contactsudhirnandhan@gmail.com>
2026-04-04 11:20:27 +0530

    feat: initialize Firebase configuration and SDK services

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,10 +6,11 @@
 const firebaseConfig = {
     apiKey: "AIzaSyABLklv7SeSay2DoLPyCPSMXH7uQ1HGAqo",
     authDomain: "iris-c3308.firebaseapp.com",
+    databaseURL: "https://iris-c3308-default-rtdb.firebaseio.com",
     projectId: "iris-c3308",
     storageBucket: "iris-c3308.firebasestorage.app",
     messagingSenderId: "64091273200",
-    appId: "1:64091273200:web:88dddf14e5413b2d17cf35"
+    appId: "1:64091273200:web:061e07e2b62e86e217cf35"
 };
 
 // Initialize Firebase (guard against double-init)
